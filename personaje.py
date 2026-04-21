7eb3c70c6c2502c54a275bb36623df02170f0758
AlejandroB <abr8805@gmail.com>
2026-04-20 20:44:44 -0500

    agregar funcion pocion en personaje

--- personaje.py
+++ personaje.py
@@ -1,9 +1,11 @@
 class Personaje:
-    def __init__(self, nombre, vida, ataque, defensa):
+    def __init__(self, nombre, vida, ataque, defensa, pociones=2):
         self.nombre = nombre
         self.vida = vida
         self.ataque = ataque
         self.defensa = defensa
+        self.pociones = pociones
+
 
     def esta_vivo(self):
         return self.vida > 0
@@ -17,7 +19,19 @@ class Personaje:
     def mostrar_estado(self):
         print(f"{self.nombre} — ❤️ Vida: {self.vida} | ⚔️ Ataque: {self.ataque} | 🛡️ Defensa: {self.defensa}")
 
+    def usar_pocion(self):
+        if self.pociones > 0:
+            curacion = 15
+            self.vida += curacion
+            self.pociones -= 1
+            print(f"🧪 {self.nombre} usa una poción y recupera {curacion} de vida")
+            print(f"   Le quedan {self.pociones} pociones")
+            self.mostrar_estado()
+        else:
+            print(f"⚠️ {self.nombre} no tiene pociones")
 
+def mostrar_estado(self):
+    print(f"{self.nombre} — ❤️ Vida: {self.vida} | ⚔️ Ataque: {self.ataque} | 🛡️ Defensa: {self.defensa} | 🧪 Pociones: {self.pociones}")
 # Probamos que funciona
 if __name__ == "__main__":
     heroe = Personaje("Héroe", vida=30, ataque=8, defensa=5)
