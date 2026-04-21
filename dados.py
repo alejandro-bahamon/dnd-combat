9cd7f6ced7cd3b0e0489560f41c3736828d746d9
AlejandroB <abr8805@gmail.com>
2026-04-20 20:20:27 -0500

    se elimina el codigo de prueba dentro del archivo dados

--- dados.py
+++ dados.py
@@ -4,13 +4,3 @@ def lanzar_dado(caras):
     """Lanza un dado de N caras y devuelve el resultado"""
     resultado = random.randint(1, caras)
     return resultado
-
-# Probamos que funciona
-if __name__ == "__main__":
-    print("🎲 Lanzando un d20...")
-    resultado = lanzar_dado(20)
-    print(f"Resultado: {resultado}")
-    
-    print("\n🎲 Lanzando un d6...")
-    resultado = lanzar_dado(6)
-    print(f"Resultado: {resultado}")
